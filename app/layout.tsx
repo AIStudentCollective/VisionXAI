2daab9e34c29f842daef5e6d7629076438536826
unknown <opinionsondemand@gmail.com>
2025-02-26 00:01:16 -0800

    added frontend to tf page

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import DeployButton from "@/components/deploy-button";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import HeaderAuth from "@/components/header-auth";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Geist } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import Link from "next/link";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+const geistSans = Geist({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+      <body className="bg-black text-white text-foreground min-h-screen min-w-full flex flex-col">
+        
+        {/* Background Wrapper */}
+        <div className="relative min-h-screen w-full flex flex-col bg-[url('/images/background.svg')] bg-cover bg-center bg-no-repeat">
+          
+          {/* Navbar */}
+          <div className="absolute top-0 left-0 w-full flex justify-between px-10 py-6">
+            <img src="/images/logo.svg" alt="Logo" className="w-60 h-60"/>
+            <div className="flex flex-row p-5 mt-20"> 
+              <Link href="" className="mx-5">Home</Link>
+              <Link href="" className="mx-5">Documents</Link>
+              <Link href="" className="mx-5">Support</Link>
+              <Link href="" className="mx-5">Privacy Policy</Link>
+            </div>
+          </div>
+
+          {/* Page Content - Adding Padding to Push Below Navbar */}
+          <div className="flex-1 flex items-center justify-center w-full h-full pt-[8rem]">
+            {children}
+          </div>
+
+        </div>
+        
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,53 @@
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default async function Home() {
+  return (
+    <>
+      <section className="flex flex-col justify-end px-16 pb-20 pt-[6rem]">
+        <div className="flex justify-end items-end w-full">
+          {/* About Section (45%) */}
+          <div className="text-white w-[45%] ml-6">
+            <h2 className="text-[48px] font-semibold mb-12">About</h2>
+            <p className="text-[20px] font-normal">
+              Lorem ipsum dolor sit amet. Eos fugiat saepe et obcaecati deserunt non enim fuga aut esse voluptas ut totam dolor 
+              hic natus quasi sed earum Quis. Et Quis officiis ea molestiae quae sit maiores reiciendis rem dicta repellat! 
+              Ea quia quidem et repellendus aperiam sit voluptatum atque in neque repudiandae a dignissimos voluptate est dolorem 
+              dolores aut fugiat dolore. Ut magni dolorem sed blanditiis molestiae qui atque deserunt.
+            </p>
+            <p className="text-[20px] font-normal mt-6">
+              Lorem ipsum dolor sit amet. Eos fugiat saepe et obcaecati deserunt non enim fuga aut esse voluptas ut totam 
+              dolor hic natus quasi sed earum Quis. Et Quis officiis ea molestiae quae sit maiores reiciendis rem dicta repellat!
+            </p>
+          </div>
+
+          {/* Empty Space (20%) */}
+          <div className="w-[20%]"></div>
+
+          {/* Framework Selection (35%) */}
+          <div className="w-[35%] flex flex-col mb-20">
+            <h3 className="text-[28px] font-semibold text-white mb-4">
+              Select A Framework
+            </h3>
+            <div className="flex gap-4">
+              <a
+                href="/torch"
+                className="w-[10rem] px-8 py-3 text-[16px] font-medium text-center bg-[linear-gradient(90deg,#9333EA_0%,#6366F1_100%)] rounded-xl shadow-md hover:opacity-80 transition-all"
+              >
+                PyTorch
+              </a>
+              <a
+                href="/tensorflow"
+                className=" w-[10rem] px-8 py-3 text-[16px] font-medium text-center bg-[linear-gradient(90deg,#9333EA_0%,#6366F1_100%)] rounded-xl shadow-md hover:opacity-80 transition-all"
+              >
+                TensorFlow
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
