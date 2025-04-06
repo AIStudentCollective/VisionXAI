8868ac0b5cf52e31d44de56b7fb6e197ef8f59f1
unknown <opinionsondemand@gmail.com>
2025-04-06 13:44:33 -0700

    updated tensorflow page

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from tensorflow import tensorflow_bp
 from pytorch import pytorch_bp
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ def home():
     return "Flask server is running!"
 
 app.register_blueprint(pytorch_bp, url_prefix='/api/pytorch')
-                       
+app.register_blueprint(tensorflow_bp, url_prefix='/api/tensorflow')
+
 if __name__ == "__main__":
     app.run(debug=True)
